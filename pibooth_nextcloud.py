d29f7d44d8ff11074105df57bdeade205928eff8
Noelle Kaczmarek <noel.kaczmarek@gmail.com>
2025-08-20 15:38:03 +0200

    feat: increase qr size

--- pibooth_nextcloud.py
+++ pibooth_nextcloud.py
@@ -175,7 +175,7 @@ def state_wait_enter(app, cfg, win):
 
         qr = qrcode.QRCode(version=1,
                         error_correction=qrcode.constants.ERROR_CORRECT_L,
-                        box_size=5,
+                        box_size=10,
                         border=2)
         qr.add_data(app.nextcloud.current_photo_link)
         qr.make(fit=True)
